417c3b3a8a1e990c907e94f64e859dc4d32c74a8
Mayan <mayan25sharma@gmail.com>
2025-12-13 01:00:47 +0530

    Final Custom K-Means

--- custom_km.py
+++ custom_km.py
@@ -1,16 +1,25 @@
 import matplotlib.pyplot as plt
-from matplotlib import style
+from sklearn import preprocessing
+from matplotlib import colors, style
 style.use('ggplot')
 import numpy as np
+import pandas as pd
 
+'''
 X = np.array([
     [1, 2],
     [1.5, 1.8],
     [5, 8],
     [8, 8],
     [1, 0.6],
-    [9, 11]
+    [9, 11],
+    [1,3],
+    [8,9],
+    [0,3],
+    [5,4],
+    [6,4],
 ])
+'''
 
 #plt.scatter(X[:, 0], X[:, 1], s=150)
 #plt.show()
@@ -45,3 +54,80 @@ class K_Means:
 
             for classification in self.classifications:
                 self.centroids[classification] = np.average(self.classifications[classification], axis = 0)
+            
+            optimized = True
+
+            for c in self.centroids:
+                original_centroid = prev_centroids[c]
+                current_centroid = self.centroids[c]
+                if np.sum((current_centroid-original_centroid)/original_centroid*100.0) > self.tol:
+                    print(np.sum((current_centroid-original_centroid)/original_centroid*100.0))
+                    optimized = False
+            
+            if optimized:
+                break
+    
+    def predict(self, data):
+        distances = [np.linalg.norm(data-self.centroids[centroid]) for centroid in self.centroids]
+        classification = distances.index(min(distances))
+        return classification
+
+df = pd.read_excel('titanic.xls')
+df.drop(['body', 'name'], axis=1, inplace=True)
+df.fillna(0, inplace=True)
+
+def handle_non_numerical_data(df):
+    
+    # handling non-numerical data: must convert.
+    columns = df.columns.values
+
+    for column in columns:
+        text_digit_vals = {}
+        def convert_to_int(val):
+            return text_digit_vals[val]
+
+        #print(column,df[column].dtype)
+        if df[column].dtype != np.int64 and df[column].dtype != np.float64:
+            
+            column_contents = df[column].values.tolist()
+            #finding just the uniques
+            unique_elements = set(column_contents)
+            # great, found them. 
+            x = 0
+            for unique in unique_elements:
+                if unique not in text_digit_vals:
+                    # creating dict that contains new
+                    # id per unique string
+                    text_digit_vals[unique] = x
+                    x+=1
+            # now we map the new "id" vlaue
+            # to replace the string. 
+            df[column] = list(map(convert_to_int,df[column]))
+
+    return df
+
+df = handle_non_numerical_data(df)
+print(df.head())
+
+# add/remove features just to see impact they have.
+df.drop(['ticket','home.dest'], axis=1, inplace=True)
+
+
+X = np.array(df.drop(['survived'], axis=1).astype(float))
+X = preprocessing.scale(X)
+y = np.array(df['survived'])
+
+clf = K_Means()
+clf.fit(X)
+
+correct = 0
+for i in range(len(X)):
+
+    predict_me = np.array(X[i].astype(float))
+    predict_me = predict_me.reshape(-1, len(predict_me))
+    prediction = clf.predict(predict_me)
+    if prediction == y[i]:
+        correct += 1
+
+
+print(correct/len(X))
