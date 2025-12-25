cf0795eb01f064c6939118dfb0171014c9bb63a1
Mayan <mayan25sharma@gmail.com>
2025-12-25 18:40:11 +0530

    Adding TensorFlow Code

---
+++ tensf.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+x1 = tf.constant(5)
+x2 = tf.constant(6)
+
+result = tf.multiply(x1, x2)
+# print(result)
+
+# sess = tf.Session()
+# # print(sess.run(result))
+# output = sess.run(result)
+# print(output)
+
+# sess.close()
+
+with tf.Session() 
