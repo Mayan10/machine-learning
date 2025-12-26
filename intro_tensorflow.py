56b63f0b649e63255f15079db09ec5fb86b2cc09
Mayan <mayan25sharma@gmail.com>
2025-12-26 22:54:44 +0530

    Adding ANN on MNIST dataset and partial Sentiment Analyzer file

--- intro_tensorflow.py
+++ intro_tensorflow.py
@@ -4,7 +4,7 @@ x1 = tf.constant(5)
 x2 = tf.constant(6)
 
 result = tf.multiply(x1, x2)
-# print(result)
+print(result)
 
 # sess = tf.Session()
 # # print(sess.run(result))
@@ -13,4 +13,8 @@ result = tf.multiply(x1, x2)
 
 # sess.close()
 
-with tf.Session() 
+# with tf.Session() as sess:
+#     output = sess.run(result)
+#     print(output)
+
+# THE COMMENTED OUT CODE IS FOR TF Version 1.x
